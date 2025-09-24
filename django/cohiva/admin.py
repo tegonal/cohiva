3e85ad1b3afd4b1319d9fe7b7e98f53375aaee52
Marco Steinacher <marco.steinacher@tegonal.com>
2025-09-24 17:45:25 +0200

    Added CohivaUnfoldConfig

---
+++ django/cohiva/admin.py
@@ -0,0 +1,8 @@
+from unfold.sites import UnfoldAdminSite
+
+
+class CohivaAdminSite(UnfoldAdminSite):
+    # site_header = "Cohiva Administration"
+    # site_title = "Cohiva Administration"
+    # index_title = "Cohiva Administration"
+    pass
