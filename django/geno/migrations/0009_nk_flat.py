f4a62eda0bee64bd9c9d8d149d29b89b65c67cf4
Alwin Egger <alwin.egger@tegonal.com>
2025-10-07 12:19:12 +0200

    merge with 'develop'

--- django/geno/migrations/0009_nk_flat.py
+++ django/geno/migrations/0009_nk_flat.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('geno', '0006_tenantsview'),
+        ('geno', '0008_migrate_emonitor2import'),
     ]
 
     operations = [
