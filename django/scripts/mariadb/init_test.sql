283727b0daff5b62ca10be54dd538ba7c40ac19e
Alwin Egger <alwin.egger@tegonal.com>
2025-11-12 16:43:55 +0100

    grant all

--- django/scripts/mariadb/init_test.sql
+++ django/scripts/mariadb/init_test.sql
@@ -1,22 +1,20 @@
--- Create main database with correct collation for MariaDB 11.4 compatibility
--- MariaDB 11.4 defaults to utf8mb4_uca1400_ai_ci, but Django/Wagtail expects utf8mb4_unicode_ci
+
 CREATE DATABASE IF NOT EXISTS `cohiva_django_test`
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
--- Create test database for automated tests (also with correct collation)
--- This database is automatically created/destroyed during test runs
 
 CREATE DATABASE IF NOT EXISTS `cohiva_gnucash_test`
     CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 
+-- Permissions in order to allow test database creation and deletion during tests
 
 -- Grant permissions
 GRANT ALL PRIVILEGES ON `cohiva_django_test`.* TO 'cohiva'@'%';
 GRANT ALL PRIVILEGES ON `cohiva_gnucash_test`.* TO 'cohiva'@'%';
 
-GRANT CREATE, DROP ON *.* TO 'cohiva'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'cohiva'@'%' WITH GRANT OPTION;
 GRANT SHOW DATABASES ON *.* TO 'cohiva'@'%';
 GRANT CREATE TABLESPACE ON *.* TO 'cohiva'@'%';
 
