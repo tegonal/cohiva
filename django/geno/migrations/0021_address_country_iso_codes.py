f8b72f6cc1b7c19ce9314525e95e856af5d8c746
Alwin Egger <alwin.egger@tegonal.com>
2026-03-17 13:08:21 +0100

    added send_info_mail and Building.country migration

--- django/geno/migrations/0021_address_country_iso_codes.py
+++ django/geno/migrations/0021_address_country_iso_codes.py
@@ -6,16 +6,28 @@ from cohiva.utils.countries import (
     normalize_country_code,
 )
 
+from geno.utils import send_info_mail
+
 COUNTRY_CHOICES = get_country_choices()
 
 
 def migrate_country_to_iso(apps, schema_editor):
     Address = apps.get_model("geno", "Address")
     for address in Address.objects.all():
-        code = normalize_country_code(address.country) or ""
+        code = normalize_country_code(address.country)
+        if code is None:
+            send_info_mail(f"Problem migrating Address.country to code", f"Could not normalize country code '{address.country}' for Address {address.pk} (setting to empty)")
+            code = ""
         address.country = code
         address.save(update_fields=["country"])
-
+    Building = apps.get_model("geno", "Building")
+    for building in Building.objects.all():
+        code = normalize_country_code(building.country)
+        if code is None:
+            send_info_mail(f"Problem migrating Building.country to code", f"Could not normalize country code '{building.country}' for Building {building.pk} (setting to empty)")
+            code = ""
+        building.country = code
+        building.save(update_fields=["country"])
 
 class Migration(migrations.Migration):
 
@@ -37,8 +49,8 @@ class Migration(migrations.Migration):
             ),
         ),
         migrations.AlterField(
-            model_name='building',
-            name='country',
+            model_name="building",
+            name="country",
             field=models.CharField(
                 "Land",
                 max_length=2,
