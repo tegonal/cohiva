a60a65d8ea028fa6074d2b6744fcdb5b86658ea2
Marco Steinacher <marco.steinacher@tegonal.com>
2026-06-28 14:24:20 +0200

    Improve Django-Filer file select widget styling.

---
+++ django/cohiva/styles/compile_styles.sh
@@ -0,0 +1,7 @@
+# See https://unfoldadmin.com/docs/styles-scripts/customizing-tailwind/
+#
+# Make sure you have Tailwind 4.x installed
+#
+#  npm i tailwindcss @tailwindcss/cli @tailwindcss/typography
+
+npx @tailwindcss/cli -i cohiva_style.css -o ../../geno/static/css/cohiva_style.css --minify
