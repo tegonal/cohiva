257b1951f2f52a611732d360bed930e84098d4e7
Alex Holloway <x4nd3r@users.noreply.github.com>
2026-06-30 16:49:05 +0200

    COHIV-206: remove patch to convert UUID fields on the fly, use 'official' upgrade path instead

--- django/geno/apps.py
+++ django/geno/apps.py
@@ -6,23 +6,13 @@ class GenoConfig(AppConfig):
 
     def ready(self):
         """Import checks when app is ready."""
-        # Django 5.2 enables native UUID on MariaDB 10.7+, but mysqlclient
-        # does not know how to serialize uuid.UUID objects. We patch Django's
-        # conversions dict to ensure that valid SQL-literal strings containing
-        # hex values are returned for every connection.
-        import uuid
-
-        import django.db.backends.mysql.base as mysql_base
-
-        from geno import checks  # noqa: F401
-
-        mysql_base.django_conversions[uuid.UUID] = lambda v, d: "'%s'" % v.hex
-
         # Delegate directly to Django's native setter for .choices, to avoid
         # an infinite recursion caused by the select2 library interacting with
         # Django 5.2's super() function in an unexpected way.
         import select2.fields
         from django.forms.fields import ChoiceField as DjangoChoiceField
+
+        from geno import checks  # noqa: F401
 
         def _select2_choices_setter(self, value):
             DjangoChoiceField.choices.fset(self, value)
---
+++ django/geno/migrations/0025_convert_uuids_from_char.py
@@ -0,0 +1,22 @@
+# Django 5.x makes use of MariaDB's UUID data type (introducted in 10.7),
+# instead of the char(32) fields that we previously used to store UUIDs. We
+# convert the existing UUID field to use the new native type, as described here:
+# https://code.djangoproject.com/ticket/33507, rather than the "official"
+# recommendation to define a custom class that leaves the char(32) columns in
+# place (https://docs.djangoproject.com/en/5.2/releases/5.0/#migrating-existing-uuidfield-on-mariadb-10-7).
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('geno', '0024_member_name_to_fk_and_date_constraint'),
+    ]
+
+    operations = [
+        # Update UUIDFields from CHAR(32) to UUID
+        migrations.RunSQL(
+            sql="""
+            ALTER TABLE geno_address MODIFY random_id UUID NOT NULL;
+            """,
+        )
+    ]
