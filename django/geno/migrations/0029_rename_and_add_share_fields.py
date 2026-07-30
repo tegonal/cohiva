79f054e765e8b0f736b67642247fea3544f58e61
Alex Holloway <x4nd3r@users.noreply.github.com>
2026-07-30 15:00:37 +0200

    COHIV-257: update migration to reflect all Share model changes

---
+++ django/geno/migrations/0029_rename_and_add_share_fields.py
@@ -0,0 +1,46 @@
+import datetime
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("geno", "0028_remove_documenttype_template_file"),
+    ]
+
+    operations = [
+        migrations.RenameField(
+            model_name="share",
+            old_name="date",
+            new_name="payment_date",
+        ),
+        migrations.RenameField(
+            model_name="share",
+            old_name="date_end",
+            new_name="repayment_date",
+        ),
+        migrations.AddField(
+            model_name='share',
+            name='effective_from',
+            field=models.DateField(blank=True, default=None, null=True, verbose_name='Wirksam ab'),
+        ),
+        migrations.AddField(
+            model_name='share',
+            name='effective_until',
+            field=models.DateField(blank=True, default=None, null=True, verbose_name='Wirksam bis'),
+        ),
+        migrations.RemoveField(
+            model_name="share", name="state"
+        ),
+        migrations.AlterField(
+            model_name='share',
+            name='payment_date',
+            field=models.DateField(default=datetime.date.today(), verbose_name='Zahlungsdatum'),
+        ),
+        migrations.AlterField(
+            model_name='share',
+            name='repayment_date',
+            field=models.DateField(blank=True, default=None, null=True, verbose_name='Rückzahlungsdatum'),
+        ),
+    ]
+
