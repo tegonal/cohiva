17dd2f44158c56e85870de62df1e983b9e2a1bb1
Alex Holloway <x4nd3r@users.noreply.github.com>
2026-08-04 13:24:16 +0200

    COHIV-305: only execute the RunSQL op during migration -- the AddField call causes an error

--- django/cohiva/utils/migrations_for_tests.py
+++ django/cohiva/utils/migrations_for_tests.py
@@ -1,6 +1,7 @@
 from importlib import import_module
 
 from django.db import DEFAULT_DB_ALIAS, connections
+from django.db.migrations import RunSQL
 from django.test.runner import DiscoverRunner
 
 
@@ -20,6 +21,7 @@ class SelectiveMigrationRunner(DiscoverRunner):
             migration_class = module.Migration
             schema_editor = connections[DEFAULT_DB_ALIAS].schema_editor()
             for op in migration_class.operations:
-                op.database_forwards(app_label, schema_editor, from_state=None, to_state=None)
+                if isinstance(op, RunSQL):
+                    op.database_forwards(app_label, schema_editor, from_state=None, to_state=None)
 
         return old_config
